e68eccfe409e4d8f7b7e4bcfd4954083d9d41be9
KeyGenQt <keygenqt@gmail.com>
2016-01-27 20:33:19 +0300

    update

--- Highcharts.php
+++ Highcharts.php
@@ -10,13 +10,9 @@ class Highcharts extends \yii\base\Widget
     public function run()
     {
         BowerAssets::register($this->getView());
-        
-        $jsOption = \yii\helpers\Html::encode($this->jsOption);
-        $jsOption = \yii\helpers\Html::encode($jsOption);
-        
-        $this->getView()->registerJs("$('#{$this->getId()}').highcharts($jsOption);");
-
-        return \yii\helpers\Html::tag('div', $this->name, \yii\helpers\ArrayHelper::merge($this->option, [
+        $this->jsOption = \yii\helpers\Json::encode($this->jsOption);
+        $this->getView()->registerJs("$('#{$this->getId()}').highcharts({$this->jsOption});");
+        return \yii\helpers\Html::tag('div', '', \yii\helpers\ArrayHelper::merge($this->option, [
             'id' => $this->getId(),
         ]));
     }
